6c3ad05568a020503f95413198f53e77f04820ea
Nikithanraj21-25 <nikithanraj.a.2021.cce@ritchennai.edu.in>
2024-04-13 23:17:39 +0530

    files

---
+++ 14-vehicle-detection-haarcascade-dl/vehicle-detection.py
@@ -0,0 +1,35 @@
+import cv2
+import imutils
+
+cascade_src = "cars.xml"
+
+car_cascade = cv2.CascadeClassifier(cascade_src)
+
+cam = cv2.VideoCapture(0)
+
+while True:
+       _, img = cam.read()
+       img = imutils.resize(img, width = 800)
+       gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+       cars = car_cascade.detectMultiScale(gray, 1.1,1)
+
+       for (x,y,w,h) in cars:
+              cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 2)
+
+       cv2.imshow("Frame", img)
+       b = str(len(cars))
+       a = int(b)
+
+       print("-------------------------------")
+       print("south  :  %d"%(a))
+       if a >=2:
+              print("South More Traffic")
+       else:
+              print("no traffic")
+
+       if cv2.waitKey(33) == 27:
+              break
+
+cam.release()
+cv2.destroyAllWindows()
